4851f33a1144a0f19bf7e0f6d682dbb3c1bd5a87
rmsndr <ramasundari.b@gmail.com>
2025-08-31 18:50:30 -0500

    Cleanup: remove stale __pycache__ and fix import path mismatch

--- utils/common_utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-def get_current_timestamp():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-def save_to_file(filename, content):
-    with open(filename, 'a') as file:
-        file.write(content + '\t' + get_current_timestamp() + '\n')
